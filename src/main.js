a84758bae6e51821507fe1ebf127ce10ad0ee3ee
maxhu787 <maxhu787@gmail.com>
2022-07-30 22:00:31 +0800

    test

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
     })
 
     win.loadFile('src/index.html')
