6f260b0facc1ac3a9e332bec81df190ce5722bc6
maxhu787 <maxhu787@gmail.com>
2022-08-02 00:13:42 +0800

    file read

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
-var remote = require('remote'); // Load remote compnent that contains the dialog dependency
-var dialog = remote.require('dialog'); // Load the dialogs component of the OS
-var fs = require('fs'); // Load the File System to execute our common tasks (CRUD)
-
-dialog.showOpenDialog((fileNames) => {
-    // fileNames is an array that contains all the selected
-    if (fileNames === undefined) {
-        console.log("No file selected");
-        return;
-    }
-
-    fs.readFile(filepath, 'utf-8', (err, data) => {
-        if (err) {
-            alert("An error ocurred reading the file :" + err.message);
-            return;
-        }
-
-        // Change how to handle the file content
-        console.log("The file content is : " + data);
-    });
-});
-
-var filepath = "C:/Previous-filepath/existinfile.txt";// you need to save the filepath when you open the file to update without use the filechooser dialog againg
-var content = "This is the new content of the file";
-
-fs.writeFile(filepath, content, (err) => {
-    if (err) {
-        alert("An error ocurred updating the file" + err.message);
-        console.log(err);
-        return;
-    }
-
-    alert("The file has been succesfully saved");
-});
+const fs = require('fs')
+const path = require('path')
+const { readTitles } = require(path.resolve('actions/uiActions'))
+readTitles('./data').map(({ title, dir }) => {
+    el = document.createElement("li");
+    text = document.createTextNode(`${title.split('.md')[0]}`);
+    el.appendChild(text)
+    el.addEventListener('click', function (e) { // clicking on sidebar titles
+        fs.readFile(dir, (err, data) => {
+            if (err) throw err;
+            fileDir = dir;
+            document.getElementById('content').innerHTML = data;
+        });
+    })
+    document.getElementById('titles').appendChild(el)
+}) 
